9f67e251eff1e5312915abdcd1177016ef844878
mdteixeira <matheusdiegoteixeira@gmail.com>
2023-05-02 11:54:04 -0300

    aula 1 de JS

---
+++ conteudo-maio/02-05/js1.js
@@ -0,0 +1,51 @@
+// Define a var nota1
+var nota_1 = 8;
+
+// Define a var nota2
+let nota_2 = 3;
+
+// Calcula a média entre a nota1 e nota2
+var media = (nota_1 + nota_2) / 2;
+
+// console.log(`A média entre ${nota_1} e ${nota_2} é ${media}`);
+
+
+// Exercício 1
+console.log("Exercício 1");
+
+var var1 = 5;
+var var2 = 8;
+
+console.log('A soma resulta em ' + (var1 + var2) + "\n");
+
+
+// Exercício 2
+console.log("Exercício 2");
+
+var var3 = 'String';
+
+console.log(var3  + "\n")
+
+
+// Exercício 3
+console.log("Exercício 3");
+
+var var4 = true;
+
+console.log(var4  + "\n" );
+
+
+// Exercício 4
+console.log("Exercício 4");
+
+nome = 'Matheus';
+sobrenome = 'Teixeira';
+
+dinheiro = 150;
+fimDeSemana = true;
+queroSair = true;
+destino = 'praia';
+
+if ((dinheiro > 100) & fimDeSemana & queroSair) {
+    console.log(`Eu, ${nome} ${sobrenome} vou sair para a ${destino}`);
+}
